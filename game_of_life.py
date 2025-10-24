50f6a55d7e224cd0fd7f4d9287126fb27b3b7d09
EL-LEITHY HUSSEIN <husseinelleithy77@gmail.com>
2025-10-24 16:57:36 +0200

    compute_next_frame logic fix

--- game_of_life.py
+++ game_of_life.py
@@ -11,20 +11,23 @@ frame = numpy.array([[0, 0, 0, 0, 0, 0, 0],
 					[0, 0, 0, 0, 0, 0, 0]])
 
 def compute_number_of_neighbors(paded_frame, index_row, index_column):
-	# Pour calculer le nombre de voisin vivant d'une cellule il suffit de sommer les valeurs de ses 8 cases voisines.
-	# Le résultat de cette somme est égale au nombre de voisins.
+	def compute_number_of_neighbors(paded_frame, index_row, index_column):
 
-	number_of_neighbors = 0
-	number_of_neighbors += paded_frame[index_row - 1, index_column - 1]
-	number_of_neighbors += paded_frame[index_row - 1, index_column]
-	number_of_neighbors += paded_frame[index_row - 1, index_column + 1]
-	number_of_neighbors += paded_frame[index_row, index_column - 1]
-	number_of_neighbors += paded_frame[index_row, index_column + 1]
-	number_of_neighbors += paded_frame[index_row + 1, index_column -1]
-	number_of_neighbors += paded_frame[index_row + 1, index_column]
-	number_of_neighbors += paded_frame[index_row + 1, index_column + 1]
+    # Pour calculer le nombre de voisin vivant d’une cellule il suffit de sommer les valeurs de ses 8 cases voisines. 
+    # Le résultat de cette somme est égale au nombre de voisins.
+    
+		number_of_neighbors = 0
+		number_of_neighbors += paded_frame[index_row - 1, index_column - 1]
+		number_of_neighbors += paded_frame[index_row - 1, index_column]
+		number_of_neighbors += paded_frame[index_row - 1, index_column + 1]
+		number_of_neighbors += paded_frame[index_row, index_column - 1]
+		number_of_neighbors += paded_frame[index_row, index_column + 1]
+		number_of_neighbors += paded_frame[index_row + 1, index_column -1]
+		number_of_neighbors += paded_frame[index_row + 1, index_column]
+		number_of_neighbors += paded_frame[index_row + 1, index_column + 1]
 
-	return number_of_neighbors
+	
+		return number_of_neighbors
 
 
 def compute_next_frame(frame):
@@ -56,5 +59,5 @@ def compute_next_frame(frame):
 while True :
 	print(frame)
 	time.sleep(1)
-	os.system("clear")
-	frame = compute_next_frame(frame)
+	os.system("cls")
+	compute_next_frame(frame)
